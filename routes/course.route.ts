95005a0e2d91362345f6c6bb1796e12da3f488a1
Sakshimehra072 <sakshimehra072@gmail.com>
2025-08-22 16:31:12 +0530

    courseChanges

--- routes/course.route.ts
+++ routes/course.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { addAnswer, addQuestion, addReplyToReview, addReview, deleteCourse, editCourse, generateVideoUrl, getAllCourses, getCourseByUser, getSingleCourse, uploadCourse } from "../controllers/course.controller";
+import { addAnswer, addQuestion, addReplyToReview, addReview, deleteCourse, editCourse, generateVideoUrl, getAdminAllCourses, getAllCourses, getCourseByUser, getSingleCourse, uploadCourse } from "../controllers/course.controller";
 import { authorizeRoles, isAuthenticated } from "../middlerware/auth";
 import { refreshTokenOptions } from "../utlis/jwt";
 import { updateAccessToken } from "../controllers/user.controller";
@@ -8,8 +8,12 @@ const courseRouter = express.Router();
 
 courseRouter.post('/create-course', updateAccessToken,isAuthenticated, authorizeRoles('admin'), uploadCourse);
 courseRouter.put('/edit-course/:id', updateAccessToken, isAuthenticated, authorizeRoles('admin'), editCourse);
+ 
 courseRouter.get('/get-course/:id', getSingleCourse);
 courseRouter.get('/get-courses', getAllCourses);
+
+courseRouter.get('/get-admin-courses',isAuthenticated, authorizeRoles("admin"), getAdminAllCourses);
+
 courseRouter.get('/get-course-content/:id', updateAccessToken , isAuthenticated, getCourseByUser);
 courseRouter.put('/add-question', updateAccessToken, isAuthenticated,addQuestion);
 courseRouter.put('/add-answer', updateAccessToken, isAuthenticated, addAnswer);
