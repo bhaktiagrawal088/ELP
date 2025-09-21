22c54f3cc6a01ee38e7b74eedd52d676e9d4cf47
Bhakti Agrawal <bhaktiagrawal397@gmail.com>
2025-09-21 19:09:00 +0530

    courseController

--- controllers/course.controller.ts
+++ controllers/course.controller.ts
@@ -60,9 +60,10 @@ export const editCourse = CatchAsyncError(
       const courseId = req.params.id;
       const courseData = (await CourseModel.findById(courseId)) as any;
 
-      if (thumbnail && !thumbnail.startsWith("https")) {
-        await cloudinary.v2.uploader.destroy(thumbnail.public_id);
-
+      if (thumbnail  && !thumbnail.startsWith("https")) {
+        if (courseData?.thumbnail?.public_id) {
+          await cloudinary.v2.uploader.destroy(courseData.thumbnail.public_id);
+        }
         const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
           folder: "courses",
         });
@@ -73,7 +74,7 @@ export const editCourse = CatchAsyncError(
         };
       }
 
-      if (thumbnail.startsWith("https")) {
+      if (thumbnail && thumbnail.startsWith("https")) {
         data.thumbnail = {
           public_id: courseData?.thumbnail.public_id,
           url: courseData?.thumbnail.url,
