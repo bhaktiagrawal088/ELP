9a8a526e6a80a62ae20e1178f82c0d5e2ccb484b
Bhakti Agrawal <bhaktiagrawal397@gmail.com>
2025-10-10 15:01:10 +0530

    /me_userRouter

--- routes/user.route.ts
+++ routes/user.route.ts
@@ -4,13 +4,11 @@ import {  authorizeRoles, isAuthenticated } from "../middlerware/auth";
 
 const userRouter = express.Router();
 userRouter.post('/registration', registrationUser);
-
 userRouter.post('/activate-user', activateUser);
 userRouter.post('/login', loginUser);
 userRouter.get('/logout', isAuthenticated,logoutUser);
 userRouter.get('/refresh', updateAccessToken);
-
-
+userRouter.get('/me', updateAccessToken, isAuthenticated, getUserInfo)
 userRouter.post('/social-auth', socialAuth);
 userRouter.put('/update-user-info', updateAccessToken, isAuthenticated, updateUserInfo);
 userRouter.put('/update-user-password',updateAccessToken, isAuthenticated,  updateUserPassword);
@@ -18,7 +16,5 @@ userRouter.put('/update-user-avatar', updateAccessToken, isAuthenticated, update
 userRouter.get('/get-all-users',updateAccessToken, isAuthenticated, authorizeRoles('admin'), getAllUsers);
 userRouter.put('/update-user-role', updateAccessToken,isAuthenticated, authorizeRoles('admin'), updateUserRole);
 userRouter.delete("/delete-user/:id", updateAccessToken, isAuthenticated, authorizeRoles('admin'), deleteUser);
-
-
 
 export default userRouter;
