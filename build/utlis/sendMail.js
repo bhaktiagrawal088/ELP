37c441c69a67f037742d5c5b90e60baa22ad8556
Bhakti Agrawal <bhaktiagrawal397@gmail.com>
2025-10-12 12:10:47 +0530

    mailersend

--- build/utlis/sendMail.js
+++ build/utlis/sendMail.js
@@ -4,33 +4,28 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 require('dotenv').config();
-const nodemailer_1 = __importDefault(require("nodemailer"));
+const mailersend_1 = require("mailersend");
 const ejs_1 = __importDefault(require("ejs"));
 const path_1 = __importDefault(require("path"));
 const sendMail = async (options) => {
-    const transporter = nodemailer_1.default.createTransport({
-        host: process.env.SMTP_HOST,
-        port: parseInt(process.env.SMTP_PORT || "587"),
-        service: process.env.SMTP_SERVICE,
-        secure: true,
-        auth: {
-            user: process.env.SMTP_MAIL,
-            pass: process.env.SMTP_PASSWORD,
-        }
-    });
-    // console.log("SMTP Mail:", process.env.SMTP_MAIL);
-    // console.log("SMTP Password:", process.env.SMTP_PASSWORD);
     const { email, subject, template, data } = options;
-    // get the path to the email template file
+    // Render the email template with EJS
     const templatePath = path_1.default.join(__dirname, "../mails", template);
-    // render the email template with EJS
     const html = await ejs_1.default.renderFile(templatePath, data);
-    const mailOptions = {
-        from: process.env.SMTP_MAIL,
-        to: email,
-        subject,
-        html
-    };
-    await transporter.sendMail(mailOptions);
+    // Initialize MailerSend
+    const mailersend = new mailersend_1.MailerSend({
+        apiKey: process.env.MAILERSEND_API_KEY,
+    });
+    // Define sender and recipient
+    const sentFrom = new mailersend_1.Sender(process.env.MAILERSEND_FROM_EMAIL, process.env.MAILERSEND_FROM_NAME || "No Name");
+    const recipients = [new mailersend_1.Recipient(email)];
+    // Define email parameters
+    const emailParams = new mailersend_1.EmailParams()
+        .setFrom(sentFrom)
+        .setTo(recipients)
+        .setSubject(subject)
+        .setHtml(html);
+    // Send the email
+    await mailersend.email.send(emailParams);
 };
 exports.default = sendMail;
--- utlis/sendMail.ts
+++ utlis/sendMail.ts
@@ -1,47 +1,44 @@
 require('dotenv').config();
-import nodemailer , {Transporter} from 'nodemailer';
+import { MailerSend, EmailParams, Sender, Recipient } from 'mailersend';
 import ejs from 'ejs';
 import path from 'path';
 
-interface EmailOptions{
-    email:string,
-    subject : string,
-    template : string,
-    data : {[key:string]:any},
+interface EmailOptions {
+    email: string;
+    subject: string;
+    template: string;
+    data: { [key: string]: any };
 }
 
-const sendMail = async(options:EmailOptions):Promise<void> => {
-    const transporter = nodemailer.createTransport({
-        host: process.env.SMTP_HOST,
-        port: parseInt(process.env.SMTP_PORT || "587"),
-        service: process.env.SMTP_SERVICE,
-        secure : true,
-        auth : {
-            user : process.env.SMTP_MAIL,
-            pass:process.env.SMTP_PASSWORD,
-        }
-    })
-    // console.log("SMTP Mail:", process.env.SMTP_MAIL);
-// console.log("SMTP Password:", process.env.SMTP_PASSWORD);
+const sendMail = async (options: EmailOptions): Promise<void> => {
+    const { email, subject, template, data } = options;
 
+    // Render the email template with EJS
+    const templatePath = path.join(__dirname, "../mails", template);
+    const html: string = await ejs.renderFile(templatePath, data);
 
-    const {email, subject, template, data} = options;
+    // Initialize MailerSend
+    const mailersend = new MailerSend({
+        apiKey: process.env.MAILERSEND_API_KEY as string,
+    });
 
-    // get the path to the email template file
-    const templatePath = path.join(__dirname,"../mails", template)
+    // Define sender and recipient
+    const sentFrom = new Sender(
+        process.env.MAILERSEND_FROM_EMAIL as string,
+        process.env.MAILERSEND_FROM_NAME || "No Name"
+    );
 
-    // render the email template with EJS
-    const html:string = await ejs.renderFile(templatePath, data);
+    const recipients = [new Recipient(email)];
 
-    const mailOptions = {
-        from : process.env.SMTP_MAIL,
-        to : email,
-        subject,
-        html
-    };
+    // Define email parameters
+    const emailParams = new EmailParams()
+        .setFrom(sentFrom)
+        .setTo(recipients)
+        .setSubject(subject)
+        .setHtml(html);
 
-    await transporter.sendMail(mailOptions)
-    
-}
+    // Send the email
+    await mailersend.email.send(emailParams);
+};
 
-export default sendMail
+export default sendMail;
